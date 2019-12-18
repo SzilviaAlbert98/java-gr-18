3eff7ddd32240f6f8ba5a1154892b14b28b10525
Albert Szilvia <albert.szilvia98@gmail.com>
2019-12-18 20:21:27 +0200

    New line

--- intro_java/src/main/java/HelloWorld.java
+++ intro_java/src/main/java/HelloWorld.java
@@ -3,9 +3,10 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello World!\n");
 
-        System.out.println(args[0]);
+        /*System.out.println(args[0]);
         System.out.println(args[1]);
-        System.out.println(args[2]);
+        System.out.println(args[2]);*/
 
+        System.out.println("Hello!");
     }
 }
